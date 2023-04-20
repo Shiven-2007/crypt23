4d43e90f3612fc041975761e1b08992557bd76e0
Shiven-2007 <shivenag@gmail.com>
2023-04-21 02:19:07 +0530

    school

--- src/app/components/schoolCode.tsx
+++ src/app/components/schoolCode.tsx
@@ -1,6 +1,14 @@
 "use client";
 import { useState } from "react";
-import {motion} from "framer-motion"
+import { motion } from "framer-motion";
+import { VT323 } from "next/font/google";
+import schoolData from "@/app/schoolData.json";
+
+const VT323Font = VT323({
+  subsets: ["latin"],
+  weight: "400",
+});
+
 async function sendData(uid: string, schoolcode: string) {
   const res = await fetch("/schoolCheck", {
     method: "POST",
@@ -16,11 +24,18 @@ const schoolCode = (props: {
   uid: string | undefined;
   schoolId: string | undefined;
 }) => {
+  const schoolsdata = schoolData;
+  console.log(
+    schoolsdata.schools.find((a) => a.schoolCode == props.schoolId),
+    props.schoolId,
+    "yoooooo"
+  );
+
   if (!props.uid) return <></>;
   if (!props.schoolId) {
     const [code, setCode] = useState("");
     const change = (e: any) => {
-      const text = e.target.value
+      const text = e.target.value;
       const allowedChars = "0123456789";
       let status = true;
       for (let i = 0; i < text.length; i++) {
@@ -32,27 +47,42 @@ const schoolCode = (props: {
         setCode(text);
       }
     };
+
     return (
       <>
-      <motion.div className="bg-zinc-800 p-3 flex flex-col rounded-xl">
-        School Code
-        <input
-          onChange={change}
-          value={code}
-          className="bg-zinc-600 p-3 rounded-sm"
+        <motion.div className="flex flex-col rounded-xl bg-zinc-800 p-3">
+          School Code
+          <input
+            onChange={change}
+            value={code}
+            className="rounded-sm bg-zinc-600 p-3"
           ></input>
-        <button onClick={() => sendData(props.uid!, code)}>submit</button>
-      </motion.div>
+          <button onClick={() => sendData(props.uid!, code)}>submit</button>
+        </motion.div>
       </>
     );
   } else {
     const code = props.schoolId;
     return (
-      <motion.div className="bg-gray-800 p-3">
-        <input value={code} disabled className="p-2"></input>
+      <motion.div
+        className={`flex flex-col justify-around rounded bg-zinc-900 p-5 ${VT323Font.className} h-1/6`}
+      >
+        <p className="text-3xl">School Code</p>
+        <input
+          value={code}
+          disabled
+          className="bg-neutral-600 p-2 text-lg tracking-widest"
+        ></input>
+        <div className="w-fit">
+          {schoolsdata.schools
+            .find((a) => a.schoolCode == props.schoolId)
+            ?.schoolName.split(", ")
+            .map((name) => (
+              <div>{name}</div>
+            ))}
+        </div>
       </motion.div>
-
-    )
+    );
   }
 };
 
