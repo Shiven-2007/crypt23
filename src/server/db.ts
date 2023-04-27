4b2934e151892cb3aefe891ddb09bb087b0a359c
Shiven-2007 <shivenag@gmail.com>
2023-04-27 18:34:08 +0530

    Update db.ts

--- src/server/db.ts
+++ src/server/db.ts
@@ -13,4 +13,4 @@ export const prisma =
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
 
-if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+globalForPrisma.prisma = prisma;
