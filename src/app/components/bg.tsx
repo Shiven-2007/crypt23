840ff6b6209645cd1732f7b21b96462afb28de27
Shiven-2007 <shivenag@gmail.com>
2023-04-27 15:39:46 +0530

    Update bg.tsx

--- src/app/components/bg.tsx
+++ src/app/components/bg.tsx
@@ -2,13 +2,16 @@
 
 function bg() {
   return (
-    <video
-      className="bgvid1"
-      src="/videos/backgroundwebsitefix.mp4"
-      autoPlay
-      muted
-      loop
-    />
+    <>
+      <video
+        className="bgvid1"
+        src="/videos/backgroundwebsitefix.mp4"
+        autoPlay
+        muted
+        loop
+      />
+      <div className="absolute left-0 top-0 h-full w-full bg-black"></div>
+    </>
   );
 }
 
