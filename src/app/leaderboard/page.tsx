d89574a73d11edbec14926a47d7397666556c3fb
Shiven-2007 <shivenag@gmail.com>
2023-04-29 23:11:17 +0530

    idkkkkk

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -6,23 +6,38 @@ const poppins = Poppins({ subsets: ["latin"], weight: ["400"] });
 const foont = Fooont({ subsets: ["latin"], weight: ["500"] });
 
 export default async function Page() {
-  const getDataa = async () => {
-    const call = await fetch("/transformers", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
+  const getData = async () => {
+    const users = await prisma.user.groupBy({
+      by: ["school_id"],
+      where: {
+        banned: {
+          equals: false,
+        },
+        school_id: {
+          not: null,
+        },
       },
+      _max: {
+        score: true,
+      },
+
+      orderBy: [
+        {
+          _max: {
+            score: "desc",
+          },
+        },
+      ],
     });
 
-    return call.json();
+    return users;
   };
-  let data1 = (await getDataa()).data;
-  console.log(data1);
+  const dat = await getData();
   return (
     <>
       <div className={"leaderboard pb-20 " + poppins.className}>
         <span className={"mb-12 text-7xl " + foont.className}>Leaderboard</span>
-        {data1.map((school: any, index: any) => (
+        {dat.map((school, index) => (
           <div
             className="leaderboard-work mx-36 flex items-center justify-between border-l-2 border-r-2 border-t-2 py-4"
             key={index}
