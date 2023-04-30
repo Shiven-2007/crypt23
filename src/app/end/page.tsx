f7d185d5f8990eb4046c2da51424da772879e8b8
Shiven-2007 <shivenag@gmail.com>
2023-04-30 23:57:43 +0530

    Revert "end page"
    This reverts commit 3ac8376e369ca7e789b2a99600a8c8023ee69f61.

--- src/app/end/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  return (
-    <div className="flex h-full items-center justify-center bg-black text-white">
-      <h1>The hunt has ended</h1>
-      <p>Stay tuned for the final leaderboard</p>
-      <p>Thank you for playing!</p>
-    </div>
-  );
-}
--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -36,16 +36,6 @@ const getData = async () => {
 };
 
 export default async function Page() {
-  const endDate = new Date("2023-05-01T00:00:00.000Z");
-  if (Date.now() > endDate.getTime()) {
-    return (
-      <div className="flex h-full items-center justify-center bg-black text-white">
-        <h1>The hunt has ended</h1>
-        <p>Stay tuned for the final leaderboard</p>
-        <p>Thank you for playing!</p>
-      </div>
-    );
-  }
   const data = await getData();
   const max = 60;
   console.log("");
